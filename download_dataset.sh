82049575cd2c38e846be33175c3579a25f3eb3fc
hzx <609100797@qq.com>
2022-03-23 09:15:31 -0400

    initial commit

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,1 +1,2 @@
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Kf93_TfEDdQaeIP3gaxh0DkdP-_zmEu-' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Kf93_TfEDdQaeIP3gaxh0DkdP-_zmEu-" -O datasets.zip && rm -rf /tmp/cookies.txt
+unzip datasets.zip && rm datasets.zip
